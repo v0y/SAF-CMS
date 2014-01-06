a48cb2b9aa18d96d5672043f6098148a205e3dde
Rafał Mirończyk <voyageur.pl@gmail.com>
2014-01-06 21:01:27 +0100

    fix urls for production

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-admin.autodiscover()
+
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
