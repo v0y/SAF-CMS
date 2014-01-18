b1494f305766b36999d5b679bf404328616bec5f
Rafał Mirończyk <voyageur.pl@gmail.com>
2014-01-19 00:53:18 +0100

    add index page object to index page

--- app/pages/views.py
+++ app/pages/views.py
@@ -1,8 +1,11 @@
 from django.views.generic.base import TemplateView
-from django.conf import settings
+
+from .models import Page
+
 
 class IndexView(TemplateView):
     template_name = 'pages/index.html'
 
     def get_context_data(self, **kwargs):
-        return {'media': settings.MEDIA_ROOT}
+        index = Page.objects.get(is_active=True, is_index=True)
+        return {'page': index}
