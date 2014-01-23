f9d997cad0140802313b288c1c3f7b0a65a0b4f0
Rafał Mirończyk <voyageur.pl@gmail.com>
2014-01-23 01:29:45 +0100

    show index and regular pages on same template

--- app/pages/urls.py
+++ app/pages/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import patterns, url
 
-from .views import IndexView, PageView
+from .views import PageView
 
 
 urlpatterns = patterns('',
-    url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^(?P<slug>[\w\d-]+)$', PageView.as_view(), name='page'),
+    url(r'^(?P<slug>[\w\d-]*)$', PageView.as_view(), name='page'),
 )
--- app/pages/views.py
+++ app/pages/views.py
@@ -3,12 +3,11 @@ from django.views.generic import DetailView
 from .models import Page
 
 
-class IndexView(DetailView):
-    template_name = 'pages/index.html'
-
-    def get_object(self, queryset=None):
-        return Page.get_index()
-
-
 class PageView(DetailView):
     model = Page
+
+    def get_object(self, queryset=None):
+        if not self.kwargs['slug']:
+            return Page.get_index()
+        else:
+            return super(PageView, self).get_object(queryset)
