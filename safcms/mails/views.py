d5d900d6b3c7594eae931ed0b62e9e88a9814085
Rafał Mirończyk <voyageur.pl@gmail.com>
2014-03-13 23:53:50 +0100

    add index page to contact form views

--- safcms/mails/views.py
+++ safcms/mails/views.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import reverse_lazy
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
 
+from safcms.pages.models import Page
 from .forms import ContactForm
 
 
@@ -10,6 +11,15 @@ class ContactFormView(FormView):
     success_url = reverse_lazy('contact_form_sent')
     template_name = 'mails/contact_form.html'
 
+    def get_context_data(self, **kwargs):
+        kwargs['object'] = Page.get_index()
+        return super(ContactFormView, self).get_context_data(**kwargs)
+
 
 class ContactFormSentView(TemplateView):
     template_name = 'mails/contact_form_sent.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['object'] = Page.get_index()
+        return super(ContactFormSentView, self).get_context_data(**kwargs)
+
