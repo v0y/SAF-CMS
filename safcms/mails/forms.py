e16a5572252766f0839868f7ef100a56f2ad8603
Rafał Mirończyk <voyageur.pl@gmail.com>
2014-03-17 19:51:08 +0100

    [3d.askra.pl] add main contact form styles

--- safcms/mails/forms.py
+++ safcms/mails/forms.py
@@ -4,5 +4,7 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=128, required=True)
     subject = forms.CharField(max_length=128, required=True)
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(
+        required=True,
+        error_messages={'invalid': "Podaj poprawny adres email"})
     text = forms.CharField(widget=forms.Textarea, required=True)
