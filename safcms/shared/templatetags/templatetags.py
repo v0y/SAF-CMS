269cd94b2773e54824b7aabfeae69f9dd63ce4ed
Rafał Mirończyk <voyageur.pl@gmail.com>
2014-03-20 00:28:53 +0100

    [3d.askra.pl] highlight active tabs

---
+++ safcms/shared/templatetags/templatetags.py
@@ -0,0 +1,33 @@
+from django import template
+from django.core.urlresolvers import NoReverseMatch, reverse
+
+register = template.Library()
+
+
+@register.simple_tag
+def is_active_tab(request, url, *params):
+    """
+    Check, if given tab is active
+
+    **Usage**::
+
+        {% is_active_tab 'url_startswith' %}
+
+    :param url: first part of url path with starting '/'
+    :param params: parameters for url
+    :return: string "active" if tab is active, else empty string
+    :rtype: str
+    """
+
+    if request.path_info.startswith(url):
+        return 'active'
+
+    try:
+        tab_url = reverse(url, args=params)
+    except NoReverseMatch:
+        return ''
+
+    if tab_url == request.path_info:
+        return 'active'
+
+    return ''
